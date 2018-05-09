8619cdfe573c4d040b6f2aa4bd5ec0d309449bd4
bassiouny <ahmedbassiouny26@gmail.com>
2018-05-09 16:08:17 +0200

    remove glide and video streaming add realm

--- app/src/main/java/ntamtech/adinz/controller/HomeController.java
+++ app/src/main/java/ntamtech/adinz/controller/HomeController.java
@@ -9,6 +9,7 @@ import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.location.LocationListener;
 
+import io.realm.Realm;
 import ntamtech.adinz.utils.LocationManager;
 
 /**
@@ -21,10 +22,19 @@ public class HomeController {
     private final int requestLocationPermission = 1;
     private Activity activity;
     private LocationListener locationListener;
+    private Realm realm;
 
     public HomeController(Activity activity) {
         this.activity = activity;
         this.locationListener = (LocationListener) activity;
+        // add realm config
+        Realm.init(activity);
+    }
+
+    public Realm getRealm(){
+        if(realm == null)
+            realm = Realm.getDefaultInstance();
+        return realm;
     }
 
     // request to access location (run time permission )
@@ -46,7 +56,7 @@ public class HomeController {
             initLocationManager();
     }
 
-    private void removeLocationListener(){
+    public void removeLocationListener(){
         locationManager.removeListener(locationListener);
     }
 
--- app/src/main/java/ntamtech/adinz/view/HomeActivity.java
+++ app/src/main/java/ntamtech/adinz/view/HomeActivity.java
@@ -1,30 +1,17 @@
 package ntamtech.adinz.view;
 
 import android.location.Location;
-import android.net.Uri;
-import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.afollestad.easyvideoplayer.EasyVideoCallback;
-import com.afollestad.easyvideoplayer.EasyVideoPlayer;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 import com.google.android.gms.location.LocationListener;
 
 import ntamtech.adinz.R;
 import ntamtech.adinz.controller.HomeController;
-import ntamtech.adinz.utils.LocationManager;
-import ntamtech.adinz.utils.MyGlideApp;
 
-public class HomeActivity extends AppCompatActivity implements LocationListener, EasyVideoCallback {
+public class HomeActivity extends AppCompatActivity implements LocationListener {
 
     // local variable
     private HomeController controller;
@@ -32,7 +19,6 @@ public class HomeActivity extends AppCompatActivity implements LocationListener,
     private final long SPLASH_TIME_OUT = 3000;
     // view
     private ImageView image;
-    private EasyVideoPlayer player;
 
 
     // override methods
@@ -59,54 +45,6 @@ public class HomeActivity extends AppCompatActivity implements LocationListener,
         getController().onRequestPermissionsResult(requestCode, grantResults);
     }
 
-    @Override
-    public void onBackPressed() {
-    }
-
-    @Override
-    public void onStarted(EasyVideoPlayer player) {
-
-    }
-
-    @Override
-    public void onPaused(EasyVideoPlayer player) {
-
-    }
-
-    @Override
-    public void onPreparing(EasyVideoPlayer player) {
-
-    }
-
-    @Override
-    public void onPrepared(EasyVideoPlayer player) {
-
-    }
-
-    @Override
-    public void onBuffering(int percent) {
-
-    }
-
-    @Override
-    public void onError(EasyVideoPlayer player, Exception e) {
-
-    }
-
-    @Override
-    public void onCompletion(EasyVideoPlayer player) {
-        // todo make request
-    }
-
-    @Override
-    public void onRetry(EasyVideoPlayer player, Uri source) {
-
-    }
-
-    @Override
-    public void onSubmit(EasyVideoPlayer player, Uri source) {
-
-    }
 
     // methods created by me
     private HomeController getController() {
@@ -120,54 +58,11 @@ public class HomeActivity extends AppCompatActivity implements LocationListener,
 
     private void initView() {
         image = findViewById(R.id.image);
-        player = findViewById(R.id.player);
     }
 
-    // this method call web service
-    // to get next ad
-    private void getAd(){
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getController().requestLocationPermission();
     }
-    // make video player hidden
-    // show image view
-    // load image with glide
-    // in case load failed i will request next ad
-    // in case load success stop app 30 sec
-    private void showImage(String url) {
-        player.setVisibility(View.GONE);
-        image.setVisibility(View.VISIBLE);
-        MyGlideApp.setImage(this, image, url, new RequestListener() {
-            @Override
-            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target target, boolean isFirstResource) {
-
-                return false;
-            }
-
-            @Override
-            public boolean onResourceReady(Object resource, Object model, Target target, DataSource dataSource, boolean isFirstResource) {
-                return false;
-            }
-        });
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                // todo make request
-                Toast.makeText(HomeActivity.this, "make request", Toast.LENGTH_SHORT).show();
-            }
-        }, SPLASH_TIME_OUT);
-    }
-
-    private void showVideo(String url){
-        // Sets the callback to this Activity, since it inherits EasyVideoCallback
-        player.setCallback(this);
-
-        // Sets the source to the HTTP URL held in the TEST_URL variable.
-        // To play files, you can use Uri.fromFile(new File("..."))
-        player.setSource(Uri.parse(url));
-        player.disableControls();
-        player.setAutoFullscreen(true);
-        player.setAutoPlay(true);
-    }
-
 }
